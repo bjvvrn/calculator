75c1cd004dbc26d28057560c6e840dff5414f99e
bjvvrn <bjvvrn@gmail.com>
2025-12-19 10:56:40 -0500

    Add operate function
    This function takes two numbers and an operator symbol as a string.
    Based on the operator, it calls one of the previously defined
    operation functions and returns the result.

--- scripts.js
+++ scripts.js
@@ -13,3 +13,16 @@ function multiply(firstNum, secondNum) {
 function divide(firstNum, secondNum) {
     return firstNum / secondNum;
 }
+
+function operate(firstNum, secondNum, operator) {
+    switch (operator) {
+        case '+':
+            return add(firstNum, secondNum);
+        case '-':
+            return subtract(firstNum, secondNum);
+        case '*':
+            return multiply(firstNum, secondNum);
+        case '/':
+            return divide(firstNum, secondNum);
+    }
+}
